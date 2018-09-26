4171fc74e4c2231d90c1f059a71026d9839843e5
Djo69 <jlebrun@edenschool.fr>
2018-09-26 13:43:40 +0200

    Menu audio

--- menu.js
+++ menu.js
@@ -1,1 +1,26 @@
 
+var backgroundAudio = (function() {
+  var audio = new Audio();
+  audio.src = './backgroundS.mp3';
+  return audio;
+})();
+
+function toggleBackgroundAudio() {
+  if (backgroundAudio.paused) {
+    backgroundAudio.play();
+  } else {
+    backgroundAudio.pause();
+    backgroundAudio.position = 0;
+  }
+}
+
+function displayNone() {
+  var i = getElementsByClassName('On')
+  && getElementsByClassName('Off');
+
+  if (i === backgroundAudio.paused) {
+    
+  } else {
+    return audio;
+  }
+}
